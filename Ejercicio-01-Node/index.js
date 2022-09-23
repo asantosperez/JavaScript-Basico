87d4792190f40b51e61bfbde3ae2fef409847de7
asantosperez <asantosperezgz@gmail.com>
2022-09-23 11:28:13 +0200

    Ejercicio 01 Node de @asantosperez: Crea un nuevo proyecto de Node, y dentro del package.json crea un script que muestre por pantalla "Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo".

---
+++ Ejercicio-01-Node/index.js
@@ -0,0 +1,4 @@
+// A mostrar por pantalla con el comando 'npm start'
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo.");
+//Firma del alumno.
+console.log("Entrega de Alberto Santos Pérez, maquetador web.");
